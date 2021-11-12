bfb32f3bb392d15c825f97ef31cc9fbf25a34419
Doris Li <dorislxy@gmail.com>
2021-11-12 13:42:14 -0800

    basic functionality with basic css

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,6 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+class WishItem(models.Model):
+    item = models.CharField(max_length=100)
--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView, DeleteView
+from .models import WishItem
 
 # Create your views here.
+class WishList(ListView):
+    model = WishItem
+
+class WishCreate(CreateView):
+    model = WishItem
+    fields = ['item']
+    success_url = '/'
+
+class WishDelete(DeleteView):
+    model = WishItem
+    success_url = '/'
